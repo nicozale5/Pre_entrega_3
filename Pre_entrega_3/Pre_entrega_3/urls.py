dd8cb7c50539627cd7c77896fc2187292a5b842a
nicozale5 <nicolas.zaleski@gmail.com>
2023-07-29 16:04:53 -0300

    Se crea clase Saludo y se modifica en urls

--- Pre_entrega_3/Pre_entrega_3/urls.py
+++ Pre_entrega_3/Pre_entrega_3/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Pre_entrega_3.views import saludar
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+path("admin/", admin.site.urls),
+path("saludo/", saludar),
 ]
