3845d5f790630ed2ae45e821e5e80069c112d19c
nicozale5 <nicolas.zaleski@gmail.com>
2023-07-31 13:23:50 -0300

    Se actualiza models y bd

--- Pre_entrega_3/control/models.py
+++ Pre_entrega_3/control/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
-
+# Se guarda class Equipo y Pais 
+"""
 class Equipo(models.Model):
     club = models.CharField(max_length=64)
 
@@ -13,14 +13,35 @@ class Pais(models.Model):
     ]
     name = models.CharField(max_length=60)
     posicion_jugador = models.CharField(max_length=2, choices=pais_equipo)
-class Jugador(models.Model):
-    apellido = models.CharField(max_length=256)
-    nombre = models.CharField(max_length=256)
-    nacionalidad = models.CharField(max_length=256)
-    año_debut = models.PositiveIntegerField()
-    fecha_nacimiento = models.DateField(null=True)
-    club = models.ManyToManyField(Equipo)
+"""
+class Pais(models.Model):
+    nombre = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nombre
+
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=100)
+    altura = models.FloatField()
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+    equipo = models.CharField(max_length=100)
+    posicion = models.CharField(max_length=50)
+    puntos_por_partido = models.DecimalField(max_digits=4, decimal_places=1)
+    asistencias_por_partido = models.DecimalField(max_digits=4, decimal_places=1)
+    rebotes_por_partido = models.DecimalField(max_digits=4, decimal_places=1)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+# Se guarda class Atributos y Posicion
+"""
 class Atributos(models.Model):
     altura = models.FloatField()  # Campo de altura
     peso = models.DecimalField(max_digits=5, decimal_places=2)  # Campo de peso
@@ -36,3 +57,4 @@ class Posicion(models.Model):
     ]
     name = models.CharField(max_length=60)
     posicion_jugador = models.CharField(max_length=2, choices=posicion_jugador)
+"""
