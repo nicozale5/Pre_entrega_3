4835305b4d6d07f4eaba75e606959207cca45ab2
nicozale5 <nicolas.zaleski@gmail.com>
2023-07-31 12:43:26 -0300

    Se actualiza models y bd

--- Pre_entrega_3/control/models.py
+++ Pre_entrega_3/control/models.py
@@ -4,23 +4,27 @@ from django.db import models
 
 class Equipo(models.Model):
     # los atributos de clase (son las columnas de la tabla)
-    nombre = models.CharField(max_length=64)
-    comision = models.IntegerField()
+    club = models.CharField(max_length=64)
+    numero = models.IntegerField()
 
 class Jugador(models.Model):
     apellido = models.CharField(max_length=256)
     nombre = models.CharField(max_length=256)
-    email = models.EmailField(blank=True)
-    telefono = models.CharField(max_length=20, blank=True)
     dni = models.CharField(max_length=32)
     fecha_nacimiento = models.DateField(null=True)
-    cursos = models.ManyToManyField(Equipo)
+    club = models.ManyToManyField(Equipo)
+
+class Atributos(models.Model):
+    peso = models.CharField(max_length=32)
+    altura = models.CharField(max_length=32)
 
 class Posicion(models.Model):
-    apellido = models.CharField(max_length=256)
-    nombre = models.CharField(max_length=256)
-    dni = models.CharField(max_length=32)
-    email = models.EmailField(blank=True)
-    fecha_nacimiento = models.DateField(null=True, blank=True)
-    profesion = models.CharField(max_length=128)
-    bio = models.TextField(blank=True)
+    posicion_jugador = [
+        ("B", "Base"),
+        ("E", "Escolta"),
+        ("A", "Ala"),
+        ("AL", "Ala-Pivot"),
+        ("P", "Pivot"),
+    ]
+    name = models.CharField(max_length=60)
+    posicion_jugador = models.CharField(max_length=2, choices=posicion_jugador)
