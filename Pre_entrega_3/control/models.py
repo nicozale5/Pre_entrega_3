8856475f55682201bb4c08df197de649496fc35b
nicozale5 <nicolas.zaleski@gmail.com>
2023-07-31 10:00:33 -0300

    Se modifica models, se crea BD

--- Pre_entrega_3/control/models.py
+++ Pre_entrega_3/control/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-class Curso(models.Model):
+class Equipo(models.Model):
     # los atributos de clase (son las columnas de la tabla)
     nombre = models.CharField(max_length=64)
     comision = models.IntegerField()
 
-class Estudiante(models.Model):
+class Jugador(models.Model):
     apellido = models.CharField(max_length=256)
     nombre = models.CharField(max_length=256)
     email = models.EmailField(blank=True)
     telefono = models.CharField(max_length=20, blank=True)
     dni = models.CharField(max_length=32)
     fecha_nacimiento = models.DateField(null=True)
-    cursos = models.ManyToManyField(Curso)
+    cursos = models.ManyToManyField(Equipo)
 
-class Profesor(models.Model):
+class Posicion(models.Model):
     apellido = models.CharField(max_length=256)
     nombre = models.CharField(max_length=256)
     dni = models.CharField(max_length=32)
