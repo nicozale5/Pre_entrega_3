60b2ab3123c0e06fe0b3ce7caa63fa856dc5240d
nicozale5 <nicolas.zaleski@gmail.com>
2023-08-02 14:57:06 -0300

    Se elimina class Saludo

--- Proyecto_Coderhouse/Pre_entrega_3/views.py
+++ Proyecto_Coderhouse/Pre_entrega_3/views.py
@@ -3,12 +3,6 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
-"""
-def saludar(request):
-    saludo = "Hola querido usuario"
-    pagina_html = HttpResponse(saludo)
-    return pagina_html
-"""
 
 def pag_inicio (request):
     http_response = render(
