7a6e8f5edf3a49ce42f7baba58d855fc7bec7a9c
nicozale5 <nicolas.zaleski@gmail.com>
2023-08-07 13:47:30 -0300

    Se agrega columna nueva en Liga y se modifica SLQite

--- Proyecto_Coderhouse/control/migrations/0001_initial.py
+++ Proyecto_Coderhouse/control/migrations/0001_initial.py
@@ -32,6 +32,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('liga', models.CharField(max_length=100)),
+                ('pais', models.CharField(max_length=100)),
             ],
         ),
         migrations.CreateModel(
